4a8981b99b9e8ff6f4bc184bfc81218005448651
yang <1827061481@qq.com>
2022-08-11 13:32:59 +0800

    2022/8/11初始版本

---
+++ webtest/src/main/java/ServletTest.java
@@ -0,0 +1,22 @@
+import javax.jws.WebService;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/test")
+public class ServletTest extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
