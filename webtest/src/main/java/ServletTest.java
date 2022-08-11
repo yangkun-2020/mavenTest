e48b720af16d2d9d7b61bf26962dde72f22204e6
yang <1827061481@qq.com>
2022-08-11 23:51:55 +0800

    master-first commit

--- webtest/src/main/java/ServletTest.java
+++ webtest/src/main/java/ServletTest.java
@@ -12,7 +12,7 @@ public class ServletTest extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        System.out.println("1");
     }
 
     @Override
